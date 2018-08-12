032b631b5b76933086a26c08e95cbed4462cd70a
michalantkowicz <michal@antkowicz.pl>
2018-08-12 16:39:42 +0200

    initial code cleanup

--- core/src/com/mantkowicz/light/board/Board.java
+++ core/src/com/mantkowicz/light/board/Board.java
@@ -1,6 +1,71 @@
 package com.mantkowicz.light.board;
 
-import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.math.Vector2;
+import com.mantkowicz.light.board.path.BoardPath;
+import com.mantkowicz.light.board.tile.Tile;
+import com.mantkowicz.light.map.TiledMapLoader;
 
-public class Board extends Group {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Board {
+    private List<Tile> tiles = new ArrayList<>();
+
+    public <T> List<Tile> loadTiles(TiledMapLoader<T> tiledMapLoader, T properties) {
+        tiles = tiledMapLoader.getTiles(properties);
+        return tiles;
+    }
+
+    public Tile getTileAt(Vector2 position) {
+        throw new UnsupportedOperationException("This operation is not implemented yet");
+    }
+
+    public BoardPath getPath(Vector2 startPosition, Vector2 endPosition) {
+        HashMap<Tile, Integer> lengthMap = new HashMap<>();
+        for (Tile tile : tiles) {
+            lengthMap.put(tile, 999999);
+        }
+
+        HashMap<Tile, Tile> prev = new HashMap<>();
+
+        Tile startTile = getTileAt(startPosition);
+        Tile endTile = getTileAt(endPosition);
+
+        lengthMap.put(startTile, 0);
+
+        List<Tile> Q = new ArrayList<>(lengthMap.keySet());
+
+        while (!Q.isEmpty()) {
+            Tile u = getNearest(lengthMap, Q);
+            for (Tile v : u.getNeighbours()) {
+                if (lengthMap.get(u) + 1 < lengthMap.get(v)) {
+                    lengthMap.put(v, lengthMap.get(u) + 1);
+                    prev.put(v, u);
+                }
+            }
+        }
+
+        List<Tile> result = new ArrayList<>();
+
+        if (lengthMap.get(endTile) < Integer.MAX_VALUE) {
+            while (prev.get(endTile) != null) {
+                result.add(endTile);
+                endTile = prev.get(endTile);
+            }
+        }
+
+        return new BoardPath(result);
+    }
+
+    private Tile getNearest(Map<Tile, Integer> lengthMap, List<Tile> q) {
+        int index = 0;
+        for (int i = 0; i < q.size(); i++) {
+            if (lengthMap.get(q.get(i)) < lengthMap.get(q.get(index))) {
+                index = i;
+            }
+        }
+        return q.remove(index);
+    }
 }
--- core/src/com/mantkowicz/light/board/tile/Tile.java
+++ core/src/com/mantkowicz/light/board/tile/Tile.java
@@ -1,16 +1,8 @@
 package com.mantkowicz.light.board.tile;
 
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.Group;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.mantkowicz.light.path.PathService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,49 +14,10 @@ public abstract class Tile extends Group {
 
     public Tile(Long id, Texture background) {
         this.id = id;
-        this.background = new Image(background);
-        addActor(this.background);
-
         this.neighbours = new ArrayList<>();
 
-        if (isAccessible()) {
-            this.addListener(createLeftClickListener());
-            this.addListener(createRightClickListener());
-        }
-
-        Label.LabelStyle style = new Label.LabelStyle();
-        style.font = new BitmapFont();
-        Label label = new Label(this.getId().toString(), style);
-        label.setPosition(30, 20);
-
-        addActor(label);
-
-    }
-
-    private EventListener createRightClickListener() {
-        return new ClickListener(Input.Buttons.RIGHT) {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                PathService.getInstance().setStartTile(Tile.this);
-                Tile.this.getBackground().setColor(0.6f, 0.8f, 0.6f, 1f);
-                super.clicked(event, x, y);
-            }
-        };
-    }
-
-    private ClickListener createLeftClickListener() {
-        return new ClickListener(Input.Buttons.LEFT) {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                PathService.getInstance().setEndTile(Tile.this);
-                List<Tile> path = PathService.getInstance().getPath();
-                for (Tile tile : path) {
-                    tile.getBackground().setColor(0.6f, 0.6f, 0.8f, 1f);
-                }
-                PathService.getInstance().reset();
-                super.clicked(event, x, y);
-            }
-        };
+        this.background = new Image(background);
+        addActor(this.background);
     }
 
     public abstract boolean isAccessible();
