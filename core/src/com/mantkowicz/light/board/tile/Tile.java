f25d3835bbcd346b4332477bd1eda23e4ecc38f7
mantkowicz <antek15@gmail.com>
2018-10-17 00:33:41 +0200

    LGM-2- refactoring

--- core/src/com/mantkowicz/light/board/tile/Tile.java
+++ core/src/com/mantkowicz/light/board/tile/Tile.java
@@ -5,38 +5,41 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Group;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.Array;
+import com.mantkowicz.light.board.tile.listener.TileClickListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Tile extends Group {
     private static final float TAN_30_DIV_6 = 0.289f;
-
+    private Color backgroundColor;
     protected Long id;
     protected List<Tile> neighbours;
-
-    public Array<Vector2> getPolygon() {
-        return polygon;
-    }
-
     protected Array<Vector2> polygon;
     protected Image background;
 
     @Override
     public void setPosition(float x, float y) {
         super.setPosition(x, y);
+        initTilePolygon();
+    }
 
+    private void initTilePolygon() {
         polygon = new Array<>();
-        polygon.add(new Vector2(getX() + (TAN_30_DIV_6 * getHeight()), getY()));
-        polygon.add(new Vector2(getX() + getWidth() - (TAN_30_DIV_6 * getHeight()), getY()));
-        polygon.add(new Vector2(getX() + getWidth(), getY() + (0.5f * getHeight())));
-        polygon.add(new Vector2(getX() + getWidth() - (TAN_30_DIV_6 * getHeight()), getY() + getHeight()));
-        polygon.add(new Vector2(getX() + (TAN_30_DIV_6 * getHeight()), getY() + getHeight()));
-        polygon.add(new Vector2(getX(), getY() + (0.5f * getHeight())));
+        float cornerOffset = TAN_30_DIV_6 * getHeight();
+        float halfOfHeight = 0.5f * getHeight();
+        polygon.add(new Vector2(getX() + cornerOffset, getY()));
+        polygon.add(new Vector2(getX() + getWidth() - cornerOffset, getY()));
+        polygon.add(new Vector2(getX() + getWidth(), getY() + halfOfHeight));
+        polygon.add(new Vector2(getX() + getWidth() - cornerOffset, getY() + getHeight()));
+        polygon.add(new Vector2(getX() + cornerOffset, getY() + getHeight()));
+        polygon.add(new Vector2(getX(), getY() + halfOfHeight));
+    }
+
+    public boolean doesContainPoint(Vector2 point) {
+        return Intersector.isPointInPolygon(this.polygon, point);
     }
 
     public Tile(Long id, Texture background) {
@@ -44,49 +47,20 @@ public abstract class Tile extends Group {
         this.neighbours = new ArrayList<>();
 
         this.background = new Image(background);
-
-
-
+        this.backgroundColor = this.background.getColor();
 
         addActor(this.background);
-//        this.background.debug();
         this.setSize(this.background.getWidth(), this.background.getHeight());
-        this.debug();
 
-        this.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                Vector2 point = new Vector2(x, y);
-                point = localToStageCoordinates(point);
-                if(Intersector.isPointInPolygon(polygon, point)) {
-                    setBlue();
-                } else {
-                    if(neighbours.isEmpty()) System.out.println("Neighbours are empty!");
-                    for (Tile neighbour : neighbours) {
-                        if(Intersector.isPointInPolygon(neighbour.getPolygon(), point)) {
-                            neighbour.setBlue();
-                        }
-                    }
-                }
-                return true;
-//                return super.touchDown(event, x, y, pointer, button);
-            }
-        });
-
-//        this.addListener(new ClickListener(){
-//            @Override
-//            public void clicked(InputEvent event, float x, float y) {
-//                System.out.println("I'm clicked!");
-//            }
-//        });
+        this.addListener(new TileClickListener(this));
     }
 
-    public void setBlue() {
+    public void mark() {
         this.background.setColor(Color.BLUE);
     }
 
-    public void setBrown() {
-        this.background.setColor(Color.BROWN);
+    public void unmark() {
+        this.background.setColor(backgroundColor);
     }
 
     public abstract boolean isAccessible();
